bb648208a2bc203694684ae05673e8d8081c6b54
mmvES <miguerepvfr@gmail.com>
2025-11-25 09:09:16 +0100

    Impelentación de ejercicio 10, correción de constructor de alumno, modificación SQL

--- src/main/java/Ejercicio5/MainApp.java
+++ src/main/java/Ejercicio5/MainApp.java
@@ -1,22 +1,20 @@
 package Ejercicio5;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
 
         AlumnoService service = new AlumnoService();
-//
-//        alumno a = new alumno("pepe", 51);
-//        service.insertarRepo(a);
-//
-//        String nombre = a.getNombre();
-//        int edad = a.getEdad();
 
-//        service.insertarDAO(nombre, edad + 2);
+        alumno a = new alumno("pepe", 51);
+        service.insertarRepo(a);
 
-//        System.out.println(service.listarRepo().toString());
+        String nombre = a.getNombre();
+        int edad = a.getEdad();
+
+        service.insertarDAO(nombre, edad + 2);
+
+        System.out.println(service.listarRepo().toString());
 
         System.out.println(service.listarDAO());
     }
